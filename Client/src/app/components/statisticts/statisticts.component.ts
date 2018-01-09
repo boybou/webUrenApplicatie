9b2aa653a685779e16e899289093e7fd099f4a10
miguelarthur <miguel92@live.nl>
2018-01-09 13:45:39 +0100

    progress

--- Client/src/app/components/statisticts/statisticts.component.ts
+++ Client/src/app/components/statisticts/statisticts.component.ts
@@ -7,28 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatistictsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {
   }
 
-  //  private myFunction() {
-  //   document.getElementById("myDropdown").classList.toggle("show");
-  // }
-  //
-  // private filterFunction() {
-  //   var input, filter, ul, li, a, i;
-  //   input = document.getElementById("myInput");
-  //   filter = input.value.toUpperCase();
-  //   div = document.getElementById("myDropdown");
-  //   a = div.getElementsByTagName("a");
-  //   for (i = 0; i < a.length; i++) {
-  //     if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-  //       a[i].style.display = "";
-  //     } else {
-  //       a[i].style.display = "none";
-  //     }
-  //   }
-  // }
+  ngOnInit() {
+
+  }
+
+
+ add(text){
+    console.log(text);
+   var hoursoff = text;
+
+  }
 
 }
