f4a34d1ffe38b24425da222c88fc0e3caf069ac9
VincentSpijkers <s1099453@student.hsleiden.nl>
2018-01-15 13:38:08 +0100

    added styling to admin and hour and user info

--- Client/src/app/components/accountmanagement/accountmanagement.component.ts
+++ Client/src/app/components/accountmanagement/accountmanagement.component.ts
@@ -9,15 +9,20 @@ import {ApiService} from "../../shared/api.service";
   styleUrls: ['./accountmanagement.component.css']
 })
 export class AccountmanagementComponent implements OnInit {
-  employee:Employee = new Employee;
-  loginData:LoginData = new LoginData;
-  checkPassword:string;
+
+  private employee:Employee = new Employee;
+  private loginData:LoginData = new LoginData;
+  private checkPassword:string;
+  private isAddAccount : boolean = true;
+  private isChangePassword : boolean = false;
+  private errorMessage : string;
+
   constructor(private api:ApiService) { }
 
   ngOnInit() {
   }
 
-  insertUser() {
+  public insertUser() {
     console.log("in insert user")
     this.employee.employee_Active = true;
     let uri = "/api/employee/insertemployee"
@@ -28,5 +33,30 @@ export class AccountmanagementComponent implements OnInit {
     let loginUri = "/api/users/insertlogindata"
     this.api.post(uri,LoginData).subscribe();
     console.log("Door logindata")
+  }
+
+  public setIsAddAccount(){
+    this.isAddAccount = true;
+    this.isChangePassword = false;
+  }
+
+  public setIsChangePassword(){
+    this.isAddAccount = false;
+    this.isChangePassword = true;
+  }
+
+  private showErrorMessagePassword(){
+    this.errorMessage = "Wachtwoorden matchen niet";
+  }
+
+  private showErrorMessageEmptyField(){
+    this.errorMessage = "Vul alle velden in";
+  }
+
+  private showErrorMessageWrongEmail(){
+    this.errorMessage = "Emailadres is niet bekend"
+  }
+
+  public changePassword(){
   }
 }
